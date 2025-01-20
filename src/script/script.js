5bd5cd20ca0b7e2b7ebfd15c5738a340ddcf75cd
Josué Santos de Matos <deevjosue@gmail.com>
2025-01-20 01:01:33 -0400

    update 1.2

--- src/script/script.js
+++ src/script/script.js
@@ -1,5 +1,5 @@
- // Lógica para abrir/fechar o dropdown
- document.addEventListener('DOMContentLoaded', function () {
+// Lógica para abrir/fechar o dropdown
+document.addEventListener('DOMContentLoaded', function () {
     const dropdown = document.getElementById('customDropdown');
     const button = dropdown.querySelector('.dropdown-button');
     const menu = dropdown.querySelector('.dropdown-menu');
@@ -32,5 +32,46 @@ function changeFontSize(action) {
         }
     });
 }
+
+
+// Seleciona o carrossel
+const carousel = document.querySelector('#cardCarousel');
+const carouselInstance = new bootstrap.Carousel(carousel);
+
+// Variáveis para rastrear o movimento
+let startX = 0;
+let endX = 0;
+
+// Evento de início do arraste
+carousel.addEventListener('mousedown', (e) => {
+    startX = e.clientX; // Posição inicial do mouse
+});
+
+// Evento durante o arraste
+carousel.addEventListener('mousemove', (e) => {
+    if (startX !== 0) {
+        endX = e.clientX; // Posição atual do mouse
+    }
+});
+
+// Evento de final do arraste
+carousel.addEventListener('mouseup', () => {
+    if (startX !== 0 && endX !== 0) {
+        // Verifica a direção do movimento
+        if (endX < startX) {
+            carouselInstance.next(); // Avança para o próximo slide
+        } else if (endX > startX) {
+            carouselInstance.prev(); // Volta para o slide anterior
+        }
+    }
+    startX = 0;
+    endX = 0;
+});
+
+// Evento para garantir que o arraste é finalizado ao sair do carrossel
+carousel.addEventListener('mouseleave', () => {
+    startX = 0;
+    endX = 0;
+});
 
 
